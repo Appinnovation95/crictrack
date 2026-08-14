565aeaea7cfc47cf7b5bde97a521b0f5bae7a839
Appinnovation95 <appinnovations.hub@gmail.com>
2026-08-14 20:28:59 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='crictrack-v5.2.2';
+const CACHE_NAME='crictrack-v5.2.3';
 const APP_SHELL=['./','./index.html','./styles.css','./ui-foundation.css','./app.js','./firebase-config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
